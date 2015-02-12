fac1df3862e318c3c07458b40968b6ef52d79a13
srikanthk <srikanth.k@motivitylabs.com>
2015-02-12 15:29:02 +0530

    Worked on Tattlez Chat and Group chat.

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
  */
 var express = require('express');
 var bodyParser = require('body-parser');
-var databaseUrl = "mongodb://srikanth:6036@proximus.modulusmongo.net:27017/zuWodo4b"; //"mongodb://srikanth:6036@proximus.modulusmongo.net:27017/zuWodo4b";
-var collections = ["users", "contacts", "communications", "messages"]; //["users","reports"]
+var databaseUrl = "mongodb://srikanth:6036@proximus.modulusmongo.net:27017/ijan4eWu";
+var collections = ["users", "contacts", "communications", "messages"];
 var mongojs = require("mongojs");
 var db = mongojs.connect(databaseUrl, collections);
 var usersData = require('./data/users');
@@ -34,17 +34,21 @@ var Chat = io
   .of('/chat')
   .on('connection', function (socket) {
   	var joinedRoom = {};
+  	var addedUser=false;
       socket.on('connected to chat', function (data) {
+          socket.username=data;
           socket.join(data);
           joinedRoom[data] = data;
+          addedUser=true;
+          console.log('connect');
+          console.log(data);
       });
       socket.on('message', function (data) {
       	if (joinedRoom[data.communicationId]) {
-              // Displays message to the sender who sent the message
-      		//socket.send(data);
-
+      		console.log('comm id');
+              console.log(joinedRoom[data.communicationId]);
       		// Broadcasts the message to the other contact
-      		socket.broadcast.to(joinedRoom[data.communicationId]).send(data.message);
+      		socket.broadcast.to(joinedRoom[data.communicationId]).send(data);
           } else {
               socket.send(
 			   "you're not connected to chat." +
@@ -52,6 +56,15 @@ var Chat = io
 			);
           }
       });
+      socket.on('disconnect', function () {
+		// remove the username from global usernames list
+		if (addedUser) {
+			socket.leave(socket.username);
+			delete joinedRoom[socket.username];
+			console.log('disconnect');
+			console.log(joinedRoom);
+		}
+	 });
   });
   /*End socket.io initialization*/
 
@@ -65,6 +78,7 @@ app.post('/contacts/deleteContact', contactsData.deleteContact(db));
 app.get('/communications/getCommunicationId/:loginNumber/:contactNumber', communicationsData.getCommunicationId(db));
 app.get('/contacts/getChatContactDetails/:contactId', contactsData.getChatContactDetails(db, mongojs));
 app.post('/messages/addMessage',messagesData.addMessage(db));
+app.post('/users/getUserByUserId/:userId',usersData.getUserByUserID(db,mongojs));
 /*Start express server*/
 var port = process.env.PORT||3000;
 server.listen(port);
