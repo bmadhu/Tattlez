b34f376b39392171bb5908cbf5741e0846248c47
bindu <bindu@adap.tv>
2015-03-27 07:23:18 +0530

    Showing all participants of Group in group Info

--- app/scripts/controllers/contactInfoController.js
+++ app/scripts/controllers/contactInfoController.js
@@ -3,10 +3,13 @@
  */
 define(['../modules/controller'], function (controllers) {
     'use strict';
-    controllers.controller('ContactInfoCtrl', function ($scope, $state, contactsSrvc, joinSrvc) {
+    controllers.controller('ContactInfoCtrl', function ($scope,$filter, $state, contactsSrvc, joinSrvc) {
         $scope.contactInfo = {};
-        // Fetches the contact Id
+        $scope.groupContactInfo = {};
+        $scope.groupContactInfoArray = [];
+            // Fetches the contact Id
         $scope.contactId = contactsSrvc.getSelectedContactForInfo();
+        var userId = joinSrvc.getUserId();
 
         //get the contact details to display
         if($scope.contactId) {
@@ -25,6 +28,25 @@ define(['../modules/controller'], function (controllers) {
                         joinSrvc.getUserByUserId(userId).then(function (userdata){
                             $scope.contactInfo.status = userdata.profileStatus;
                         });
+                    });
+                } else {
+                    var allContactsArray = JSON.parse(localStorage.getItem(userId + '_C'));
+                    var groupContacts = $scope.contactInfo.contactNumber.split(',');
+                    /**
+                     * For group show all the participants of the group
+                     * If the group member exists in contacts list show his name
+                     * If not show the contact number for name also.
+                     */
+                    angular.forEach(groupContacts,function(v,k){
+                        var obj = {};
+                        var contact = $filter('filter')(allContactsArray,{contactNumber: v},true)[0];
+                        if(contact)
+                            obj.contactName = contact.contactName;
+                        else
+                            obj.contactName =  v;
+                        obj.contactNumber = v;
+                        obj.photo = (contact && contact.photo) ? contact.photo : "../images/default_profile_M.jpg";
+                        $scope.groupContactInfoArray.push(obj);
                     });
                 }
             });
